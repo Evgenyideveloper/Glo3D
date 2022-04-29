d67b3514492d0d29ba772301c1b13d4256f976ee
Vtornikov Evgeny <phtor@mail.ru>
2022-04-29 20:25:35 +0300

    lesson21

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import calculation from './modules/calculation';
+import validateForm from './modules/validateForm';
 
 
 timer('3 may 2022');
 menu();
 modal();
+calculation();
+validateForm();
---
+++ src/modules/validateForm.js
@@ -0,0 +1,32 @@
+const validateForm = () =>{
+	const forms = document.querySelectorAll('form');
+	
+	
+	forms.forEach(function(item){
+		const typeText = item.querySelector('input[type=text]');	
+			typeText.addEventListener('input', (e)=>{
+				e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\s-]/, "")
+			});
+			
+		const typeEmail = item.querySelector('input[type=email]');	
+			typeEmail.addEventListener('input', (e)=>{
+				e.target.value = e.target.value.replace(/[^a-zA-Z0-9*~!'_@.-]/, "")
+			});
+			
+		const typeTel = item.querySelector('input[type=tel]');	
+			typeTel.addEventListener('input', (e)=>{
+				e.target.value = e.target.value.replace(/[^0-9()-]/, "")
+			});
+			
+		const placeholder = document.querySelector('.mess');	
+			placeholder.addEventListener('input', (e)=>{
+				e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\s-]/, "")
+			});			
+			
+	}); 
+	
+	
+
+}
+
+export default validateForm;
